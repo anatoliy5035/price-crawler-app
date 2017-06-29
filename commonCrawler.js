e1fc6ac3b4032f845a55122cdcc8d3c8531a3b21
Anatoliy <a.bygyb@gmail.com>
2017-06-29 14:22:21 +0300

    refactoring

---
+++ commonCrawler.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const osmosis = require('osmosis');
+
+
+function commonCrawler(cartUrl, name, divBlock, priceElement) {
+    osmosis
+        .get(cartUrl)
+        .find(divBlock)
+        .set({
+            'price': priceElement
+        })
+        .log(console.log)
+        .data(function(data) {
+            fs.writeFile(name + '-data.json', JSON.stringify(data, null, 4), function(err) {
+                if(err) console.error(err);
+                else console.log('Data Saved to data.json file');
+            })
+        })
+}
+
+module.exports = commonCrawler;
--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-let europosud = require('./europosud');
-let hmstore = require('./hmstore');
-let joseph = require('./josephjoseph');
-let joseph_ua = require('./josephjoseph-ua');
+const commonCrawler = require('./commonCrawler');
+const dataUrl = require('./cart-url/cart-data.json');
+
+function getCrawlerById(id) {
+    let idLoc = id.toString();
+    return dataUrl.filter(x => x.id === idLoc);
+}
+
+function runCrawler(website) {
+    const name = website[0].name;
+    const divBlock = website[0].divBlock;
+    const priceElement = website[0].priceElement;
+    const url = website[0].url;
+
+    commonCrawler(url, name, divBlock, priceElement);
+}
+
+let hmstore = getCrawlerById(1);
+let europosud = getCrawlerById(2);
+let josephjoseph_ua = getCrawlerById(3);
+let josephjoseph = getCrawlerById(4);
+
+
+runCrawler(hmstore);
+runCrawler(europosud);
+runCrawler(josephjoseph_ua);
+runCrawler(josephjoseph);
+
 
