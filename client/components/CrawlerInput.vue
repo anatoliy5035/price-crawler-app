da12193182db5373f1dc4ecbae3b35010c2c67af
Anatoliy <a.bygyb@gmail.com>
2017-07-09 14:33:53 +0300

    add validation

--- client/components/CrawlerInput.vue
+++ client/components/CrawlerInput.vue
@@ -2,9 +2,16 @@
     <div class="container">
         <h1 class="main-title text-center">Crawler</h1>
         <div class="form-group">
-            <label for="url">product url:</label>
-            <input type="text" class="form-control url-input" id="url" v-model="urlData">
-            <button type="submit" id="submitInputButton" class="btn btn-primary" v-on:click="getDomainName()">GET</button>
+            <form @submit.prevent="getDomainName">
+                <input type="text"
+                       placeholder="product url:"
+                       name="url"
+                       class="form-control url-input" id="url"
+                       v-validate="'required'"
+                       v-model="urlData"
+                       :class="{'input': true, 'is-danger': errors.has('url') }">
+                <button type="submit" id="submitInputButton" class="btn btn-primary">GET</button>
+            </form>
         </div>
         <ul class="list-group">
             <li class="list-group-item" v-for="response in serverText">{{response.domain}}<span class="badge">{{response.price}}</span></li>
@@ -31,16 +38,26 @@ export default {
 //    },
     methods: {
             getDomainName() {
-                this.$http.post('/getPriceFromUrl', {url: this.urlData})
-                    .then(res => {
-                            this.serverText.push(res.body);
-                    })
+                this.$validator.validateAll().then(result => {
+                    if (result) {
+                        this.$http.post('/getPriceFromUrl', {url: this.urlData})
+                            .then(res => {
+                                this.serverText.push(res.body);
+                            });
+                        return;
+                    }
+               return;
+                });
             }
     }
 }
 </script>
 
 <style scoped>
+    .is-danger {
+        border-color: #c91b1b;
+    }
+
     a {
         color: #00B7FF;
     }
--- client/index.js
+++ client/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import vueResource from 'vue-resource';
 import App from './components/App.vue';
+import VeeValidate from 'vee-validate';
+
 
 Vue.use(vueResource);
+Vue.use(VeeValidate);
 
-Vue.http.interceptors.push((request, next) => {
-	request.headers.set('x-access-token', localStorage.getItem('session'));
-	request.headers.set('Accept', 'application/json');
-	next();
-});
+// Vue.http.interceptors.push((request, next) => {
+// 	request.headers.set('x-access-token', localStorage.getItem('session'));
+// 	request.headers.set('Accept', 'application/json');
+// 	next();
+// });
 
 new Vue({
 	el: '#app',
